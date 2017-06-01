fee32866c300787ee95d2d81b6289cc18f477cd3
FloFAU <flowurmer@gmx.de>
2017-06-01 14:44:23 +0200

    Testsystem_mini (latest version)

---
+++ genEq_mini.m
@@ -0,0 +1,15 @@
+function F = genEq(x,M,anzahl_zweige,Loesungsvektor)
+
+c=x; % c = vector of variables, needs to match the size of inputted solutions-vector
+
+for i = 1:size(x) % fills vector c with x(1) ... x(N)// just as an example to test if the system works..
+    c(i) = x(i);
+end
+
+for i = anzahl_zweige+1:2*anzahl_zweige % the volume flow is a square function
+ c(i) = x(i)^2;
+end
+    
+F = ((M*c)-Loesungsvektor);  % creates functions that have to be solved (F(x)=0)
+
+
