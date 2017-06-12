c8edc7b0302ac16336b9afecbe970fff71d6be46
FloFAU <flowurmer@gmx.de>
2017-06-12 14:45:15 +0200

    Create test_linsolve.m

--- test_linsolve.m
+++ test_linsolve.m
@@ -37,9 +37,10 @@ A = [E_Z,-Z,-E_Z;null_one,middle_A,side_A;-V_U,-Z_V,E_Z];
 
 p = zeros(b,1)
 p(1) = 20000
-rhs_middle = [0;0;0;B*p]
+v_rhs_middle = zeros(n-1,1) % new
+rhs_middle = [v_rhs_middle;B*p] % Zeros are not always zeros -> has been optimized
 rhs_end = zeros(b,1)
 
-r_k = [p;rhs_middle;rhs_end]
+r_k = [-p;rhs_middle;rhs_end] % "p" changed to "-p" 
 
 x = linsolve(A,r_k)
